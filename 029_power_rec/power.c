bc3764e3931000bbb0276b8c3b899eb059e3323a
ys319 <ys319@duke.edu>
2020-09-09 13:21:46 +0800

    answer

--- 029_power_rec/power.c
+++ 029_power_rec/power.c
@@ -1,7 +1,20 @@
 unsigned power(unsigned x, unsigned y){
-  if(y == 0 || y == 1){
+  if( y == 1){
     return x;
   }
+  
+  if(x == 0 && y == 0){
+  
+  return 1;
+  
+  
+  }
+  
+  if( y == 0){
+    return 1;
+  }
+  
+  
 
   return x * power(x, y - 1);
 }
