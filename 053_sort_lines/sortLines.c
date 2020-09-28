3738ae0cf82977208f2f51ca449f6340fdba234d
ys319 <ys319@duke.edu>
2020-09-28 10:56:25 +0800

    answer

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -16,9 +16,62 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void printArr(FILE * f){
+   size_t size = 0;
+    char ** myArr = NULL;
+  char * line = NULL;
+  size_t len = 0;
+  
+   while(getline(&line, &len, f) >= 0){
+       size++;
+       myArr =realloc( myArr, size * sizeof(*myArr));
+       myArr[size - 1] = line;
+       line = NULL;
+       
+   }
+    
+     
+     sortData(myArr, size);
+
+  
+  for (size_t i = 0; i < size; i++){
+    printf("%s", myArr[i]);
+    }
+
+    free(myArr);
+
+}
 int main(int argc, char ** argv) {
   
   //WRITE YOUR CODE HERE!
+ 
+  if (argc == 1) {
+      FILE* f = stdin;
+      if (f == NULL){
+       fprintf(stderr,"count not find the file ");
+      return EXIT_FAILURE;
+      }
+      printArr(f);   
+  }else{
+    for(size_t i = 1; i < argc; i++ ){
+      FILE *f = fopen(argv[i], "r");
+
+      if (f == NULL){
+    fprintf(stderr,"count not find the file ");
+     return EXIT_FAILURE;
+     
+  }
+      printArr(f);
+      if (fclose(f) != 0) {
+        perror("Failed to close the input file!");
+        return EXIT_FAILURE;
+      }
+
+
+    }
+    
+
+  }
   
   return EXIT_SUCCESS;
 }
