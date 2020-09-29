19e25d91dd4e81eeecdf204b50af0060c14df57b
ys319 <ys319@duke.edu>
2020-09-29 14:45:17 +0800

    answer

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -5,4 +5,12 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  const char * suffix = ".counts";
+  unsigned len = strlen(inputName) + strlen(suffix) + 1;
+  char * ans = malloc(len * sizeof(*ans));
+  strcpy(ans, inputName);
+  strcpy((ans+strlen(inputName)), suffix);
+  // ans[strlen(inputName)] = suffix;
+  return ans;
+
 }
