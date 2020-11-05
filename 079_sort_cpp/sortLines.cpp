1f043596e3c07720764f128d2d371c04491963c5
ys319 <ys319@duke.edu>
2020-11-05 22:46:52 +0800

    answer

---
+++ 079_sort_cpp/sortLines.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+
+void printVec(std::vector<std::string> vec){
+    for (size_t i = 0; i < vec.size(); i++){
+        std::cout << vec[i] << std::endl;
+    }
+}
+
+int main(int argc, char ** argv){
+    std::vector<std::string> vec;
+    std::string str;
+    std::ifstream file;
+    if(argc == 1){
+        while(!std::cin.eof()){
+            std::getline(std::cin, str);
+            vec.push_back(str);
+        }
+    }else{
+        for (size_t i = 1; i < argc; i++){
+            file.open(argv[i]);
+            if(file.fail()){
+                std::cerr << "fail to open file" << std::endl;
+            }
+            while(!file.eof()){
+                std::getline(file, str);
+                vec.push_back(str);
+            }
+            file.close();
+        }
+    }
+    std::sort(vec.begin(), vec.end());
+    printVec(vec);
+    return 0;
+}
