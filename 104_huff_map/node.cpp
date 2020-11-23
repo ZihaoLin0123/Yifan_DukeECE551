158fc276d158cfd32760f3ee1674caea85c05070
ys319 <ys319@duke.edu>
2020-11-23 18:19:43 +0800

    asnwer

--- 104_huff_map/node.cpp
+++ 104_huff_map/node.cpp
@@ -1,5 +1,16 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
+#include <assert.h>
+using namespace std;
+void Node::buildMap(BitString b, std::map<unsigned, BitString> &theMap)
+{
   //WRITE ME!
+  if (sym != NO_SYM){
+    assert(left == NULL && right == NULL);
+    theMap.insert(pair<unsigned, BitString>(sym, b));
+  }else{
+    assert(left != NULL && right != NULL);
+    left->buildMap(b.plusZero(), theMap);
+    right->buildMap(b.plusOne(), theMap);
+  }
 }
 
