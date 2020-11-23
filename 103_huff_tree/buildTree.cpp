608e1803b35f27c63a27ddebcc80168bd5921912
ys319 <ys319@duke.edu>
2020-11-23 16:43:55 +0800

    asnwer

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,28 @@
 #include "node.h"
+#include <stdio.h>
+#include <cstdlib>
 
 Node *  buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  unsigned index = 0;
+  while(index <= 256){
+    if(counts[index] != 0){
+      pq.push(new Node(index, counts[index]));
+    }
+    ++index;
+  }
+  Node *node1;
+  Node *node2;
+
+  while(pq.size() > 1){
+    node1 = pq.top();
+    pq.pop();
+    node2 = pq.top();
+    pq.pop();
+    Node *parentNode = new Node(node1, node2);
+    pq.push(parentNode);
+  }
+  return pq.top();
+}
+
